cbcfe55c5dfb085d1427976456ba538c1bd46535
unknown <mortazavi.ali1994@gmail.com>
2019-04-19 19:09:21 +0430

    new files

--- HW2/test.py
+++ HW2/test.py
@@ -1,13 +1,4 @@
 import numpy as np
 import matplotlib.pyplot as plt
-a = np.array([2,4])
-b = np.array([7,8])
-a = np.arange(9).reshape(3,3)
-# print (a)
-# b = a[:,[1,2]]
-# print (b)
-for i in range(0, 3):
-    for j in range(0,3):
-        if j==1:
-            break
-    print ("Hello")
+risk_array = np.zeros([2,3])
+print (risk_array)
